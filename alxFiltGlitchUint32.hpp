8e6d15afe02ba01ebf61be45f68988f14375f2ba
tomazvidovic <tomaz@tomazvidovic.com>
2022-09-18 02:31:11 +0200

    fixed file headers to C++

--- alxFiltGlitchUint32.hpp
+++ alxFiltGlitchUint32.hpp
@@ -8,20 +8,20 @@
   *
   * SPDX-License-Identifier: GPL-3.0-or-later
   *
-  * This file is part of Auralix C Library.
+  * This file is part of Auralix C++ Library.
   *
-  * Auralix C Library is free software: you can redistribute it and/or
+  * Auralix C++ Library is free software: you can redistribute it and/or
   * modify it under the terms of the GNU General Public License
   * as published by the Free Software Foundation, either version 3
   * of the License, or (at your option) any later version.
   *
-  * Auralix C Library is distributed in the hope that it will be useful,
+  * Auralix C++ Library is distributed in the hope that it will be useful,
   * but WITHOUT ANY WARRANTY; without even the implied warranty of
   * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
   * GNU General Public License for more details.
   *
   * You should have received a copy of the GNU General Public License
-  * along with Auralix C Library. If not, see <https://www.gnu.org/licenses/>.
+  * along with Auralix C++ Library. If not, see <https://www.gnu.org/licenses/>.
   ******************************************************************************
   **/
 
