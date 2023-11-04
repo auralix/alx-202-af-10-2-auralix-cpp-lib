63f74e8bfdcb21e975561769db5072cf9b943901
tomazvidovic <tomaz@tomazvidovic.com>
2023-11-04 17:46:02 +0100

    added alxFs & alxParamKvStore

---
+++ alxFs.hpp
@@ -0,0 +1,101 @@
+﻿/**
+  ******************************************************************************
+  * @file		alxFs.hpp
+  * @brief		Auralix C++ Library - ALX File System Module
+  * @copyright	Copyright (C) 2020-2022 Auralix d.o.o. All rights reserved.
+  *
+  * @section License
+  *
+  * SPDX-License-Identifier: GPL-3.0-or-later
+  *
+  * This file is part of Auralix C++ Library.
+  *
+  * Auralix C++ Library is free software: you can redistribute it and/or
+  * modify it under the terms of the GNU General Public License
+  * as published by the Free Software Foundation, either version 3
+  * of the License, or (at your option) any later version.
+  *
+  * Auralix C++ Library is distributed in the hope that it will be useful,
+  * but WITHOUT ANY WARRANTY; without even the implied warranty of
+  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+  * GNU General Public License for more details.
+  *
+  * You should have received a copy of the GNU General Public License
+  * along with Auralix C++ Library. If not, see <https://www.gnu.org/licenses/>.
+  ******************************************************************************
+  **/
+
+//******************************************************************************
+// Include Guard
+//******************************************************************************
+#ifndef ALX_FS_HPP
+#define ALX_FS_HPP
+
+
+//******************************************************************************
+// Includes
+//******************************************************************************
+#include "alxGlobal.hpp"
+#include "alxFs.h"
+
+
+//******************************************************************************
+// Module Guard
+//******************************************************************************
+#if defined(ALX_CPP_LIB)
+
+
+//******************************************************************************
+// Code
+//******************************************************************************
+namespace Alx
+{
+	namespace AlxFs
+	{
+		//******************************************************************************
+		// Class - IFs
+		//******************************************************************************
+		class IFs
+		{
+			public:
+				//------------------------------------------------------------------------------
+				// Public Functions
+				//------------------------------------------------------------------------------
+				IFs() {}
+				virtual ~IFs() {}
+				virtual int32_t Mount(void) = 0;
+				virtual int32_t UnMount(void) = 0;
+				virtual int32_t Format(void) = 0;
+				virtual int32_t Remove(const char* path) = 0;
+//				virtual int32_t FileOpen(AlxFs_File* file, const char* path, int32_t flags) = 0;	// TODO
+//				virtual int32_t FileClose(AlxFs_File* file) = 0;
+//				virtual int32_t FileRead(AlxFs_File* file, void* buff, uint32_t len) = 0;
+//				virtual int32_t FileWrite(AlxFs_File* file, void* buff, uint32_t len) = 0;
+				virtual ::AlxFs* GetCStructPtr(void) = 0;
+		};
+
+
+		//******************************************************************************
+		// Class - Fs
+		//******************************************************************************
+		class Fs : public IFs
+		{
+			public:
+				//------------------------------------------------------------------------------
+				// Public Functions
+				//------------------------------------------------------------------------------
+				// TODO
+
+			private:
+				//------------------------------------------------------------------------------
+				// Private Variables
+				//------------------------------------------------------------------------------
+				::AlxFs me = {};
+		};
+	}
+}
+
+
+#endif	// #if defined(ALX_CPP_LIB)
+
+#endif	// #ifndef ALX_FS_HPP
---
+++ alxParamKvStore.hpp
@@ -0,0 +1,134 @@
+﻿/**
+  ******************************************************************************
+  * @file		alxParamKvStore.hpp
+  * @brief		Auralix C++ Library - ALX Parameter Key-Value Store Module
+  * @copyright	Copyright (C) 2020-2022 Auralix d.o.o. All rights reserved.
+  *
+  * @section License
+  *
+  * SPDX-License-Identifier: GPL-3.0-or-later
+  *
+  * This file is part of Auralix C++ Library.
+  *
+  * Auralix C++ Library is free software: you can redistribute it and/or
+  * modify it under the terms of the GNU General Public License
+  * as published by the Free Software Foundation, either version 3
+  * of the License, or (at your option) any later version.
+  *
+  * Auralix C++ Library is distributed in the hope that it will be useful,
+  * but WITHOUT ANY WARRANTY; without even the implied warranty of
+  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+  * GNU General Public License for more details.
+  *
+  * You should have received a copy of the GNU General Public License
+  * along with Auralix C++ Library. If not, see <https://www.gnu.org/licenses/>.
+  ******************************************************************************
+  **/
+
+//******************************************************************************
+// Include Guard
+//******************************************************************************
+#ifndef ALX_PARAM_KV_STORE_HPP
+#define ALX_PARAM_KV_STORE_HPP
+
+
+//******************************************************************************
+// Includes
+//******************************************************************************
+#include "alxGlobal.hpp"
+#include "alxParamKvStore.h"
+#include "alxFs.hpp"
+
+
+//******************************************************************************
+// Module Guard
+//******************************************************************************
+#if defined(ALX_CPP_LIB)
+
+
+//******************************************************************************
+// Code
+//******************************************************************************
+namespace Alx
+{
+	namespace AlxParamKvStore
+	{
+		//******************************************************************************
+		// Class - IParamKvStore
+		//******************************************************************************
+		class IParamKvStore
+		{
+			public:
+				//------------------------------------------------------------------------------
+				// Public Functions
+				//------------------------------------------------------------------------------
+				IParamKvStore() {}
+				virtual ~IParamKvStore() {}
+				virtual Alx_Status Init(void) = 0;
+				virtual Alx_Status DeInit(void) = 0;
+				virtual Alx_Status Get(const char* key, void* buff, uint32_t len, uint32_t* actualLen) = 0;
+				virtual Alx_Status Set(const char* key, void* buff, uint32_t len) = 0;
+				virtual Alx_Status Remove(const char* key) = 0;
+				virtual ::AlxParamKvStore* GetCStructPtr(void) = 0;
+		};
+
+
+		//******************************************************************************
+		// Class - ParamKvStore
+		//******************************************************************************
+		class ParamKvStore : public IParamKvStore
+		{
+			public:
+				//------------------------------------------------------------------------------
+				// Public Functions
+				//------------------------------------------------------------------------------
+				ParamKvStore
+				(
+					AlxFs::IFs* fs
+				)
+				{
+					AlxParamKvStore_Ctor
+					(
+						&me,
+						fs->GetCStructPtr()
+					);
+				}
+				virtual ~ParamKvStore() {}
+				Alx_Status Init(void) override
+				{
+					return AlxParamKvStore_Init(&me);
+				}
+				Alx_Status DeInit(void) override
+				{
+					return AlxParamKvStore_DeInit(&me);
+				}
+				Alx_Status Get(const char* key, void* buff, uint32_t len, uint32_t* actualLen) override
+				{
+					return AlxParamKvStore_Get(&me, key, buff, len, actualLen);
+				}
+				Alx_Status Set(const char* key, void* buff, uint32_t len) override
+				{
+					return AlxParamKvStore_Set(&me, key, buff, len);
+				}
+				Alx_Status Remove(const char* key) override
+				{
+					return AlxParamKvStore_Remove(&me, key);
+				}
+				::AlxParamKvStore* GetCStructPtr(void) override
+				{
+					return &me;
+				}
+
+			private:
+				//------------------------------------------------------------------------------
+				// Private Variables
+				//------------------------------------------------------------------------------
+				::AlxParamKvStore me = {};
+		};
+	}
+}
+
+
+#endif	// #if defined(ALX_CPP_LIB)
+
+#endif	// #ifndef ALX_PARAM_KV_STORE_HPP
