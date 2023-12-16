9d59cc6ddd8f4b7383b23044bd704291f3178a39
tomazvidovic <tomaz@tomazvidovic.com>
2023-12-16 16:23:48 +0100

    alxI2c - removed all predefined function parameters

--- Mcu/alxI2c.hpp
+++ Mcu/alxI2c.hpp
@@ -67,15 +67,15 @@ namespace Alx
 				virtual ~II2c() {}
 				virtual Alx_Status Init(void) = 0;
 				virtual Alx_Status DeInit(void) = 0;
-				virtual Alx_Status Master_StartRead(uint16_t slaveAddr, uint8_t* data, uint16_t len = 1, uint16_t timeout_ms = 10) = 0;
-				virtual Alx_Status Master_StartReadStop(uint16_t slaveAddr, uint8_t* data, uint16_t len = 1, uint8_t numOfTries = 3, uint16_t timeout_ms = 10) = 0;
-				virtual Alx_Status Master_StartReadMemStop(uint16_t slaveAddr, uint16_t memAddr, AlxI2c_Master_MemAddrLen memAddrLen, uint8_t* data, uint16_t len = 1, uint8_t numOfTries = 3, uint16_t timeout_ms = 10) = 0;
-				virtual Alx_Status Master_StartWrite(uint16_t slaveAddr, const uint8_t* data, uint16_t len = 1, uint16_t timeout_ms = 10) = 0;
-				virtual Alx_Status Master_StartWriteStop(uint16_t slaveAddr, const uint8_t* data, uint16_t len = 1, uint8_t numOfTries = 3, uint16_t timeout_ms = 10) = 0;
-				virtual Alx_Status Master_StartWriteMemStop(uint16_t slaveAddr, uint16_t memAddr, AlxI2c_Master_MemAddrLen memAddrLen, uint8_t data, bool checkWithRead = false, uint8_t numOfTries = 3, uint16_t timeout_ms = 10) = 0;
-				virtual Alx_Status Master_StartWriteMemStop(uint16_t slaveAddr, uint16_t memAddr, AlxI2c_Master_MemAddrLen memAddrLen, const uint8_t* data, uint16_t len, bool checkWithRead = false, uint8_t numOfTries = 3, uint16_t timeout_ms = 10) = 0;
-				virtual Alx_Status Master_Stop(uint16_t timeout_ms = 10) = 0;
-				virtual Alx_Status Master_IsSlaveReady(uint16_t slaveAddr, uint8_t numOfTries = 3, uint16_t timeout_ms = 10) = 0;
+				virtual Alx_Status Master_StartRead(uint16_t slaveAddr, uint8_t* data, uint16_t len, uint16_t timeout_ms) = 0;
+				virtual Alx_Status Master_StartReadStop(uint16_t slaveAddr, uint8_t* data, uint16_t len, uint8_t numOfTries, uint16_t timeout_ms) = 0;
+				virtual Alx_Status Master_StartReadMemStop(uint16_t slaveAddr, uint16_t memAddr, AlxI2c_Master_MemAddrLen memAddrLen, uint8_t* data, uint16_t len, uint8_t numOfTries, uint16_t timeout_ms) = 0;
+				virtual Alx_Status Master_StartWrite(uint16_t slaveAddr, const uint8_t* data, uint16_t len, uint16_t timeout_ms) = 0;
+				virtual Alx_Status Master_StartWriteStop(uint16_t slaveAddr, const uint8_t* data, uint16_t len, uint8_t numOfTries, uint16_t timeout_ms) = 0;
+				virtual Alx_Status Master_StartWriteMemStop(uint16_t slaveAddr, uint16_t memAddr, AlxI2c_Master_MemAddrLen memAddrLen, uint8_t data, bool checkWithRead, uint8_t numOfTries, uint16_t timeout_ms) = 0;
+				virtual Alx_Status Master_StartWriteMemStop(uint16_t slaveAddr, uint16_t memAddr, AlxI2c_Master_MemAddrLen memAddrLen, const uint8_t* data, uint16_t len, bool checkWithRead, uint8_t numOfTries, uint16_t timeout_ms) = 0;
+				virtual Alx_Status Master_Stop(uint16_t timeout_ms) = 0;
+				virtual Alx_Status Master_IsSlaveReady(uint16_t slaveAddr, uint8_t numOfTries, uint16_t timeout_ms) = 0;
 				virtual ::AlxI2c* GetCStructPtr(void) = 0;
 		};
 
@@ -99,39 +99,39 @@ namespace Alx
 				{
 					return AlxI2c_DeInit(&me);
 				}
-				Alx_Status Master_StartRead(uint16_t slaveAddr, uint8_t* data, uint16_t len = 1, uint16_t timeout_ms = 10) override
+				Alx_Status Master_StartRead(uint16_t slaveAddr, uint8_t* data, uint16_t len, uint16_t timeout_ms) override
 				{
 					return AlxI2c_Master_StartRead(&me, slaveAddr, data, len, timeout_ms);
 				}
-				Alx_Status Master_StartReadStop(uint16_t slaveAddr, uint8_t* data, uint16_t len = 1, uint8_t numOfTries = 3, uint16_t timeout_ms = 10) override
+				Alx_Status Master_StartReadStop(uint16_t slaveAddr, uint8_t* data, uint16_t len, uint8_t numOfTries, uint16_t timeout_ms) override
 				{
 					return AlxI2c_Master_StartReadStop(&me, slaveAddr, data, len, numOfTries, timeout_ms);
 				}
-				Alx_Status Master_StartReadMemStop(uint16_t slaveAddr, uint16_t memAddr, AlxI2c_Master_MemAddrLen memAddrLen, uint8_t* data, uint16_t len = 1, uint8_t numOfTries = 3, uint16_t timeout_ms = 10) override
+				Alx_Status Master_StartReadMemStop(uint16_t slaveAddr, uint16_t memAddr, AlxI2c_Master_MemAddrLen memAddrLen, uint8_t* data, uint16_t len, uint8_t numOfTries, uint16_t timeout_ms) override
 				{
 					return AlxI2c_Master_StartReadMemStop(&me, slaveAddr, memAddr, memAddrLen, data, len, numOfTries, timeout_ms);
 				}
-				Alx_Status Master_StartWrite(uint16_t slaveAddr, const uint8_t* data, uint16_t len = 1, uint16_t timeout_ms = 10) override
+				Alx_Status Master_StartWrite(uint16_t slaveAddr, const uint8_t* data, uint16_t len, uint16_t timeout_ms) override
 				{
 					return AlxI2c_Master_StartWrite(&me, slaveAddr, data, len, timeout_ms);
 				}
-				Alx_Status Master_StartWriteStop(uint16_t slaveAddr, const uint8_t* data, uint16_t len = 1, uint8_t numOfTries = 3, uint16_t timeout_ms = 10) override
+				Alx_Status Master_StartWriteStop(uint16_t slaveAddr, const uint8_t* data, uint16_t len, uint8_t numOfTries, uint16_t timeout_ms) override
 				{
 					return AlxI2c_Master_StartWriteStop(&me, slaveAddr, data, len, numOfTries, timeout_ms);
 				}
-				Alx_Status Master_StartWriteMemStop(uint16_t slaveAddr, uint16_t memAddr, AlxI2c_Master_MemAddrLen memAddrLen, uint8_t data, bool checkWithRead = false, uint8_t numOfTries = 3, uint16_t timeout_ms = 10) override
+				Alx_Status Master_StartWriteMemStop(uint16_t slaveAddr, uint16_t memAddr, AlxI2c_Master_MemAddrLen memAddrLen, uint8_t data, bool checkWithRead, uint8_t numOfTries, uint16_t timeout_ms) override
 				{
 					return AlxI2c_Master_StartWriteMemStop_Single(&me, slaveAddr, memAddr, memAddrLen, data, checkWithRead, numOfTries, timeout_ms);
 				}
-				Alx_Status Master_StartWriteMemStop(uint16_t slaveAddr, uint16_t memAddr, AlxI2c_Master_MemAddrLen memAddrLen, const uint8_t* data, uint16_t len, bool checkWithRead = false, uint8_t numOfTries = 3, uint16_t timeout_ms = 10) override
+				Alx_Status Master_StartWriteMemStop(uint16_t slaveAddr, uint16_t memAddr, AlxI2c_Master_MemAddrLen memAddrLen, const uint8_t* data, uint16_t len, bool checkWithRead, uint8_t numOfTries, uint16_t timeout_ms) override
 				{
 					return AlxI2c_Master_StartWriteMemStop_Multi(&me, slaveAddr, memAddr, memAddrLen, data, len, checkWithRead, numOfTries, timeout_ms);
 				}
-				Alx_Status Master_Stop(uint16_t timeout_ms = 10) override
+				Alx_Status Master_Stop(uint16_t timeout_ms) override
 				{
 					return AlxI2c_Master_Stop(&me, timeout_ms);
 				}
-				Alx_Status Master_IsSlaveReady(uint16_t slaveAddr, uint8_t numOfTries = 3, uint16_t timeout_ms = 10)override
+				Alx_Status Master_IsSlaveReady(uint16_t slaveAddr, uint8_t numOfTries, uint16_t timeout_ms)override
 				{
 					return AlxI2c_Master_IsSlaveReady(&me, slaveAddr, numOfTries, timeout_ms);
 				}
