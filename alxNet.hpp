dc4e16769fcd0cadd0af71288598e476e0ee262e
andrej strancar <andrej.strancar@jung-electronic.de>
2024-05-18 09:57:03 +0200

    adding wiznet

--- alxNet.hpp
+++ alxNet.hpp
@@ -37,7 +37,8 @@
 //******************************************************************************
 #include "alxGlobal.hpp"
 #include "alxNet.h"
-
+#include "alxSpi.hpp"
+#include "alxIoPin.h"
 
 //******************************************************************************
 // Module Guard
@@ -62,7 +63,23 @@ namespace Alx
 				// Public Functions
 				//------------------------------------------------------------------------------
 				INet() {}
-				virtual ~INet() {}
+				virtual ~INet() {}		
+				virtual Alx_Status Init(void) = 0;
+				virtual Alx_Status Connect(void) = 0;
+				virtual Alx_Status Disconnect(void) = 0;
+				virtual bool IsConnected(void) = 0;
+				virtual void SetMac(const char* mac) = 0;
+				virtual void SetIp(const char* ip) = 0;
+				virtual void SetNetmask(const char* netmask) = 0;
+				virtual void SetGateway(const char* gateway) = 0;
+				virtual const char* GetMac(void) = 0;
+				virtual const char* GetIp(void) = 0;
+				virtual const char* GetNetmask(void) = 0;
+				virtual const char* GetGateway(void) = 0;
+				virtual void Dns_SetIp(uint8_t dnsId, const char* ip) = 0;
+				virtual Alx_Status Dns_GetHostByName(const char* hostname, char* ip) = 0;
+				virtual void Dhcp_Enable(bool enable) = 0;
+				virtual bool Dhcp_WasAddrSupplied(void) = 0;	
 		};
 
 
@@ -77,17 +94,208 @@ namespace Alx
 				//------------------------------------------------------------------------------
 				Net
 				(
-					AlxNet_Config config
+					AlxNet_Config config,
+					AlxSpi::ISpi* alxSpi,
+					AlxIoPin::IIoPin* do_nRST,
+					AlxIoPin::IIoPin* di_nINT
 				)
 				{
 					AlxNet_Ctor
 					(
 						&me,
-						config
+						config,
+						alxSpi->GetCStructPtr(),
+						do_nRST->GetCStructPtr(),
+						di_nINT->GetCStructPtr()
 					);
 				}
 				virtual ~Net() {}
-
+				Alx_Status Init(void) override
+				{
+					if (me.config == AlxNet_Config_Wiznet)
+					{
+						return (AlxNet_Init(&me));
+					}
+					else if (me.config == AlxNet_Config_Wiznet)
+					{
+						// return (modem...);
+					}
+					return Alx_Err;
+				}
+				Alx_Status Connect(void) override
+				{
+					if (me.config == AlxNet_Config_Wiznet)
+					{
+						return (AlxNet_Connect(&me));
+					}
+					else if (me.config == AlxNet_Config_Wiznet)
+					{
+						// return (modem...);
+					}
+					return Alx_Err;
+				}
+				Alx_Status Disconnect(void) override
+				{
+					if (me.config == AlxNet_Config_Wiznet)
+					{
+						return (AlxNet_Disconnect(&me));
+					}
+					else if (me.config == AlxNet_Config_Wiznet)
+					{
+						// return (modem...);
+					}
+					return Alx_Err;
+				}
+				bool IsConnected(void) override
+				{
+					if (me.config == AlxNet_Config_Wiznet)
+					{
+						return (AlxNet_IsConnected(&me));
+					}
+					else if (me.config == AlxNet_Config_Wiznet)
+					{
+						// return (modem...);
+					}
+					return false;
+				}
+				void SetMac(const char* mac) override
+				{
+					if(me.config == AlxNet_Config_Wiznet)
+					{
+						AlxNet_SetMac(&me, mac);
+					}
+					else if (me.config == AlxNet_Config_Wiznet)
+					{
+						// (modem...);
+					}
+				}
+				void SetIp(const char* ip) override
+				{
+					if(me.config == AlxNet_Config_Wiznet)
+					{
+						AlxNet_SetIp(&me, ip) ;
+					}
+					else if(me.config == AlxNet_Config_Wiznet)
+					{
+						// (modem...);
+					}
+				}
+				void SetNetmask(const char* netmask) override
+				{
+					if (me.config == AlxNet_Config_Wiznet)
+					{
+						AlxNet_SetNetmask(&me, netmask);
+					}
+					else if (me.config == AlxNet_Config_Wiznet)
+					{
+						// (modem...);
+					}
+				}
+				void SetGateway(const char* gateway) override
+				{
+					if (me.config == AlxNet_Config_Wiznet)
+					{
+						AlxNet_SetGateway(&me, gateway);
+					}
+					else if (me.config == AlxNet_Config_Wiznet)
+					{
+						// (modem...);
+					}
+				}
+				const char* GetMac(void) override
+				{
+					if (me.config == AlxNet_Config_Wiznet)
+					{
+						return AlxNet_GetMac(&me);
+					}
+					else if (me.config == AlxNet_Config_Wiznet)
+					{
+						// return (modem...);
+					}
+					return NULL;
+				}
+				const char* GetIp(void) override
+				{
+					if (me.config == AlxNet_Config_Wiznet)
+					{
+						return AlxNet_GetIp(&me);
+					}
+					else if (me.config == AlxNet_Config_Wiznet)
+					{
+						// return (modem...);
+					}
+					return NULL;
+				}
+				const char* GetNetmask(void) override
+				{
+					if (me.config == AlxNet_Config_Wiznet)
+					{
+						return AlxNet_GetNetmask(&me);
+					}
+					else if (me.config == AlxNet_Config_Wiznet)
+					{
+						// return (modem...);
+					}
+					return NULL;
+				}
+				const char* GetGateway(void) override
+				{
+					if (me.config == AlxNet_Config_Wiznet)
+					{
+						return AlxNet_GetGateway(&me);
+					}
+					else if (me.config == AlxNet_Config_Wiznet)
+					{
+						// return (modem...);
+					}
+					return NULL;
+				}
+				void Dns_SetIp(uint8_t dnsId, const char* ip) override
+				{
+					if (me.config == AlxNet_Config_Wiznet)
+					{
+						AlxNet_Dns_SetIp(&me, dnsId, ip);
+					}
+					else if (me.config == AlxNet_Config_Wiznet)
+					{
+						// (modem...);
+					}
+				}
+				Alx_Status Dns_GetHostByName(const char* hostname, char* ip) override
+				{
+					if (me.config == AlxNet_Config_Wiznet)
+					{
+						return AlxNet_Dns_GetHostByName(&me, hostname, ip);
+					}
+					else if (me.config == AlxNet_Config_Wiznet)
+					{
+						// return (modem...);
+					}
+					return Alx_Err;
+				}
+				void Dhcp_Enable(bool enable) override
+				{
+					if (me.config == AlxNet_Config_Wiznet)
+					{
+						AlxNet_Dhcp_Enable(&me, enable);
+					}
+					else if (me.config == AlxNet_Config_Wiznet)
+					{
+						// (modem...);
+					}
+				}
+				bool Dhcp_WasAddrSupplied(void) override
+				{
+					if (me.config == AlxNet_Config_Wiznet)
+					{
+						return AlxNet_Dhcp_WasAddrSupplied(&me);
+					}
+					else if (me.config == AlxNet_Config_Wiznet)
+					{
+						// return (modem...);
+					}
+					return false;
+				}
 			private:
 				//------------------------------------------------------------------------------
 				// Private Variables
