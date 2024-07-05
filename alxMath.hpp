034b45dabeb4f44ceb5e393e88e7da4c6df8d4fd
tomazvidovic <tomaz@tomazvidovic.com>
2024-07-05 20:25:01 +0200

    improving alxLogger, added alxMath

---
+++ alxMath.hpp
@@ -0,0 +1,101 @@
+﻿/**
+  ******************************************************************************
+  * @file		alxMath.hpp
+  * @brief		Auralix C++ Library - ALX Hysteresis Module
+  * @copyright	Copyright (C) Auralix d.o.o. All rights reserved.
+  *
+  * @section License
+  *
+  * SPDX-License-Identifier: GPL-3.0-or-later
+  *
+  * This file is part of Auralix C++ Library.
+  *
+  * Auralix C++ Library is free software: you can redistribute it and/or
+  * modify it under the terms of the GNU General Public License
+  * as published by the Free Software Foundation, either version 3
+  * of the License, or (at your option) any later version.
+  *
+  * Auralix C++ Library is distributed in the hope that it will be useful,
+  * but WITHOUT ANY WARRANTY; without even the implied warranty of
+  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+  * GNU General Public License for more details.
+  *
+  * You should have received a copy of the GNU General Public License
+  * along with Auralix C++ Library. If not, see <https://www.gnu.org/licenses/>.
+  ******************************************************************************
+  **/
+
+//******************************************************************************
+// Include Guard
+//******************************************************************************
+#ifndef ALX_MATH_HPP
+#define ALX_MATH_HPP
+
+
+//******************************************************************************
+// Includes
+//******************************************************************************
+#include "alxGlobal.hpp"
+#include "alxMath.h"
+
+
+//******************************************************************************
+// Module Guard
+//******************************************************************************
+#if defined(ALX_CPP_LIB)
+
+
+//******************************************************************************
+// Code
+//******************************************************************************
+namespace Alx
+{
+	namespace AlxMath
+	{
+		//******************************************************************************
+		// Class - IMath
+		//******************************************************************************
+		class IMath
+		{
+			public:
+				//------------------------------------------------------------------------------
+				// Public Functions
+				//------------------------------------------------------------------------------
+				IMath() {}
+				virtual ~IMath() {}
+				virtual AlxMath_Data Process(float in) = 0;
+		};
+
+
+		//******************************************************************************
+		// Class - Math
+		//******************************************************************************
+		class Math : public IMath
+		{
+			public:
+				//------------------------------------------------------------------------------
+				// Public Functions
+				//------------------------------------------------------------------------------
+				Math()
+				{
+					AlxMath_Ctor(&me);
+				}
+				virtual ~Math() {}
+				AlxMath_Data Process(float in) override
+				{
+					return AlxMath_Process(&me, in);
+				}
+
+			private:
+				//------------------------------------------------------------------------------
+				// Private Variables
+				//------------------------------------------------------------------------------
+				::AlxMath me = {};
+		};
+	}
+}
+
+
+#endif	// #if defined(ALX_CPP_LIB)
+
+#endif	// #ifndef ALX_MATH_HPP
