3938885271bdf0331fa89cbc95e3a5d88f369eac
tomazvidovic <tomaz@tomazvidovic.com>
2025-03-29 17:09:34 +0100

    alxLogger - Organize functions into sections

--- alxLogger.hpp
+++ alxLogger.hpp
@@ -61,32 +61,106 @@ namespace Alx
 		{
 			public:
 				//------------------------------------------------------------------------------
+				//------------------------------------------------------------------------------
 				// Public Functions
+				//------------------------------------------------------------------------------
 				//------------------------------------------------------------------------------
 				ILogger() {}
 				virtual ~ILogger() {}
+				virtual ::AlxLogger* GetCStructPtr(void) = 0;
+
+
+
+				//------------------------------------------------------------------------------
+				//------------------------------------------------------------------------------
+				// General
+				//------------------------------------------------------------------------------
+				//------------------------------------------------------------------------------
+
+
+				//------------------------------------------------------------------------------
+				// Setup
+				//------------------------------------------------------------------------------
 				virtual Alx_Status Init(void) = 0;
 				virtual Alx_Status Format(void) = 0;
-				virtual Alx_Status Read(char* logs, uint32_t numOfLogs, uint32_t* numOfLogsActual, bool mdUpdate, uint64_t idStart) = 0;
-				virtual Alx_Status ReadFile(const char* path, uint8_t* chunkBuff, uint32_t chunkLen, Alx_Status(*chunkRead_Callback)(void* chunkData, uint32_t chunkLenActual)) = 0;
-				virtual Alx_Status ReadFileFirstLine(const char* path, char* log) = 0;
-				virtual Alx_Status Write(const char* logs, uint32_t numOfLogs) = 0;
-				virtual Alx_Status GetFileSize(const char* path, uint32_t* fileSize) = 0;
-				virtual uint64_t GetNumOfLogsToProcess(void) = 0;
-				virtual Alx_Status DiscardLogsToProcess(void) = 0;
-				virtual Alx_Status RewindLogsToProcessFiles(uint32_t numOfFiles) = 0;
-				virtual Alx_Status ForwardLogsToProcessFiles(uint32_t numOfFiles) = 0;
-				virtual uint64_t GetNumOfLogsStored(void) = 0;
-				virtual Alx_Status GetIdOldest(uint64_t* idOldest) = 0;
-				virtual Alx_Status GetIdNewest(uint64_t* idNewest) = 0;
-				virtual Alx_Status GetFilePathOldest(char* filePathOldest) = 0;
-				virtual Alx_Status GetFilePathNewest(char* filePathNewest) = 0;
+
+
+				//------------------------------------------------------------------------------
+				// Metadata
+				//------------------------------------------------------------------------------
 				virtual AlxLogger_Metadata GetMetadataCurrent(void) = 0;
 				virtual AlxLogger_Metadata GetMetadataStored(void) = 0;
 				virtual Alx_Status StoreMetadata(AlxLogger_StoreMetadata_Config config) = 0;
+
+
+				//------------------------------------------------------------------------------
+				// Debug
+				//------------------------------------------------------------------------------
 				virtual AlxMath_Data GetMath_Data_ReadTime_ms(void) = 0;
 				virtual AlxMath_Data GetMath_Data_WriteTime_ms(void) = 0;
-				virtual ::AlxLogger* GetCStructPtr(void) = 0;
+
+
+
+
+				//------------------------------------------------------------------------------
+				//------------------------------------------------------------------------------
+				// Log Level
+				//------------------------------------------------------------------------------
+				//------------------------------------------------------------------------------
+
+
+				//------------------------------------------------------------------------------
+				// Read/Write
+				//------------------------------------------------------------------------------
+				virtual Alx_Status Read(char* logs, uint32_t numOfLogs, uint32_t* numOfLogsActual, bool mdUpdate, uint64_t idStart) = 0;
+				virtual Alx_Status Write(const char* logs, uint32_t numOfLogs) = 0;
+
+
+				//------------------------------------------------------------------------------
+				// Status
+				//------------------------------------------------------------------------------
+				virtual uint64_t GetNumOfLogsToProcess(void) = 0;
+				virtual uint64_t GetNumOfLogsStored(void) = 0;
+				virtual Alx_Status GetIdOldest(uint64_t* idOldest) = 0;
+				virtual Alx_Status GetIdNewest(uint64_t* idNewest) = 0;
+
+
+				//------------------------------------------------------------------------------
+				// Management
+				//------------------------------------------------------------------------------
+				virtual Alx_Status DiscardLogsToProcess(void) = 0;
+
+
+
+
+				//------------------------------------------------------------------------------
+				//------------------------------------------------------------------------------
+				// File Level
+				//------------------------------------------------------------------------------
+				//------------------------------------------------------------------------------
+
+
+				//------------------------------------------------------------------------------
+				// Read/Write
+				//------------------------------------------------------------------------------
+				virtual Alx_Status ReadFile(const char* path, uint8_t* chunkBuff, uint32_t chunkLen, Alx_Status(*chunkRead_Callback)(void* chunkData, uint32_t chunkLenActual)) = 0;
+				virtual Alx_Status ReadFileFirstLine(const char* path, char* log) = 0;
+
+
+
+				//------------------------------------------------------------------------------
+				// Status
+				//------------------------------------------------------------------------------
+				virtual Alx_Status GetFileSize(const char* path, uint32_t* fileSize) = 0;
+				virtual Alx_Status GetFilePathOldest(char* filePathOldest) = 0;
+				virtual Alx_Status GetFilePathNewest(char* filePathNewest) = 0;
+
+
+				//------------------------------------------------------------------------------
+				// Management
+				//------------------------------------------------------------------------------
+				virtual Alx_Status RewindLogsToProcessFiles(uint32_t numOfFiles) = 0;
+				virtual Alx_Status ForwardLogsToProcessFiles(uint32_t numOfFiles) = 0;
 		};
 
 
@@ -97,7 +171,9 @@ namespace Alx
 		{
 			public:
 				//------------------------------------------------------------------------------
+				//------------------------------------------------------------------------------
 				// Public Functions
+				//------------------------------------------------------------------------------
 				//------------------------------------------------------------------------------
 				Logger
 				(
@@ -136,6 +212,24 @@ namespace Alx
 					);
 				}
 				virtual ~Logger() {}
+				::AlxLogger* GetCStructPtr(void) override
+				{
+					return &me;
+				}
+
+
+
+
+				//------------------------------------------------------------------------------
+				//------------------------------------------------------------------------------
+				// General
+				//------------------------------------------------------------------------------
+				//------------------------------------------------------------------------------
+
+
+				//------------------------------------------------------------------------------
+				// Setup
+				//------------------------------------------------------------------------------
 				Alx_Status Init(void) override
 				{
 					return AlxLogger_Init(&me);
@@ -144,41 +238,66 @@ namespace Alx
 				{
 					return AlxLogger_Format(&me);
 				}
+
+
+				//------------------------------------------------------------------------------
+				// Metadata
+				//------------------------------------------------------------------------------
+				AlxLogger_Metadata GetMetadataCurrent(void) override
+				{
+					return AlxLogger_GetMetadataCurrent(&me);
+				}
+				AlxLogger_Metadata GetMetadataStored(void) override
+				{
+					return AlxLogger_GetMetadataStored(&me);
+				}
+				Alx_Status StoreMetadata(AlxLogger_StoreMetadata_Config config) override
+				{
+					return AlxLogger_StoreMetadata(&me, config);
+				}
+
+
+				//------------------------------------------------------------------------------
+				// Debug
+				//------------------------------------------------------------------------------
+				AlxMath_Data GetMath_Data_ReadTime_ms(void) override
+				{
+					return AlxLogger_GetMath_Data_ReadTime_ms(&me);
+				}
+				AlxMath_Data GetMath_Data_WriteTime_ms(void) override
+				{
+					return AlxLogger_GetMath_Data_WriteTime_ms(&me);
+				}
+
+
+
+
+				//------------------------------------------------------------------------------
+				//------------------------------------------------------------------------------
+				// Log Level
+				//------------------------------------------------------------------------------
+				//------------------------------------------------------------------------------
+
+
+				//------------------------------------------------------------------------------
+				// Read/Write
+				//------------------------------------------------------------------------------
 				Alx_Status Read(char* logs, uint32_t numOfLogs, uint32_t* numOfLogsActual, bool mdUpdate, uint64_t idStart) override
 				{
 					return AlxLogger_Read(&me, logs, numOfLogs, numOfLogsActual, mdUpdate, idStart);
-				}
-				Alx_Status ReadFile(const char* path, uint8_t* chunkBuff, uint32_t chunkLen, Alx_Status(*chunkRead_Callback)(void* chunkData, uint32_t chunkLenActual)) override
-				{
-					return AlxLogger_ReadFile(&me, path, chunkBuff, chunkLen, chunkRead_Callback);
-				}
-				Alx_Status ReadFileFirstLine(const char* path, char* log) override
-				{
-					return AlxLogger_ReadFileFirstLine(&me, path, log);
 				}
 				Alx_Status Write(const char* logs, uint32_t numOfLogs) override
 				{
 					return AlxLogger_Write(&me, logs, numOfLogs);
 				}
+
+
+				//------------------------------------------------------------------------------
+				// Status
+				//------------------------------------------------------------------------------
 				uint64_t GetNumOfLogsToProcess(void) override
 				{
 					return AlxLogger_GetNumOfLogsToProcess(&me);
-				}
-				Alx_Status DiscardLogsToProcess(void) override
-				{
-					return AlxLogger_DiscardLogsToProcess(&me);
-				}
-				Alx_Status RewindLogsToProcessFiles(uint32_t numOfFiles) override
-				{
-					return AlxLogger_RewindLogsToProcessFiles(&me, numOfFiles);
-				}
-				Alx_Status ForwardLogsToProcessFiles(uint32_t numOfFiles) override
-				{
-					return AlxLogger_ForwardLogsToProcessFiles(&me, numOfFiles);
-				}
-				Alx_Status GetFileSize(const char* path, uint32_t* fileSize) override
-				{
-					return AlxLogger_GetFileSize(&me, path, fileSize);
 				}
 				uint64_t GetNumOfLogsStored(void) override
 				{
@@ -192,6 +311,46 @@ namespace Alx
 				{
 					return AlxLogger_GetIdNewest(&me, idNewest);
 				}
+
+
+				//------------------------------------------------------------------------------
+				// Management
+				//------------------------------------------------------------------------------
+				Alx_Status DiscardLogsToProcess(void) override
+				{
+					return AlxLogger_DiscardLogsToProcess(&me);
+				}
+
+
+
+
+				//------------------------------------------------------------------------------
+				//------------------------------------------------------------------------------
+				// File Level
+				//------------------------------------------------------------------------------
+				//------------------------------------------------------------------------------
+
+
+				//------------------------------------------------------------------------------
+				// Read/Write
+				//------------------------------------------------------------------------------
+				Alx_Status ReadFile(const char* path, uint8_t* chunkBuff, uint32_t chunkLen, Alx_Status(*chunkRead_Callback)(void* chunkData, uint32_t chunkLenActual)) override
+				{
+					return AlxLogger_ReadFile(&me, path, chunkBuff, chunkLen, chunkRead_Callback);
+				}
+				Alx_Status ReadFileFirstLine(const char* path, char* log) override
+				{
+					return AlxLogger_ReadFileFirstLine(&me, path, log);
+				}
+
+
+				//------------------------------------------------------------------------------
+				// Status
+				//------------------------------------------------------------------------------
+				Alx_Status GetFileSize(const char* path, uint32_t* fileSize) override
+				{
+					return AlxLogger_GetFileSize(&me, path, fileSize);
+				}
 				Alx_Status GetFilePathOldest(char* filePathOldest) override
 				{
 					return AlxLogger_GetFilePathOldest(&me, filePathOldest);
@@ -200,34 +359,27 @@ namespace Alx
 				{
 					return AlxLogger_GetFilePathNewest(&me, filePathNewest);
 				}
-				AlxLogger_Metadata GetMetadataCurrent(void) override
+
+
+				//------------------------------------------------------------------------------
+				// Management
+				//------------------------------------------------------------------------------
+				Alx_Status RewindLogsToProcessFiles(uint32_t numOfFiles) override
 				{
-					return AlxLogger_GetMetadataCurrent(&me);
+					return AlxLogger_RewindLogsToProcessFiles(&me, numOfFiles);
 				}
-				AlxLogger_Metadata GetMetadataStored(void) override
+				Alx_Status ForwardLogsToProcessFiles(uint32_t numOfFiles) override
 				{
-					return AlxLogger_GetMetadataStored(&me);
+					return AlxLogger_ForwardLogsToProcessFiles(&me, numOfFiles);
 				}
-				Alx_Status StoreMetadata(AlxLogger_StoreMetadata_Config config) override
-				{
-					return AlxLogger_StoreMetadata(&me, config);
-				}
-				AlxMath_Data GetMath_Data_ReadTime_ms(void) override
-				{
-					return AlxLogger_GetMath_Data_ReadTime_ms(&me);
-				}
-				AlxMath_Data GetMath_Data_WriteTime_ms(void) override
-				{
-					return AlxLogger_GetMath_Data_WriteTime_ms(&me);
-				}
-				::AlxLogger* GetCStructPtr(void) override
-				{
-					return &me;
-				}
+
+
 
 			private:
 				//------------------------------------------------------------------------------
+				//------------------------------------------------------------------------------
 				// Private Variables
+				//------------------------------------------------------------------------------
 				//------------------------------------------------------------------------------
 				::AlxLogger me = {};
 		};
