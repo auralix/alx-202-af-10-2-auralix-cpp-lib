aa565f4ae2eecca828c9b6a8183eebf33301446f
tomazvidovic <tomaz@tomazvidovic.com>
2025-04-17 22:19:39 +0200

    AlxFtp_Client_ChangeDir - improved

--- alxFtp.hpp
+++ alxFtp.hpp
@@ -74,7 +74,7 @@ namespace Alx
 				virtual void Client_SetExtraFileHeader(const char* extraFileHeader) = 0;
 				virtual Alx_Status Client_Login(void) = 0;
 				virtual Alx_Status Client_Logout(void) = 0;
-				virtual Alx_Status Client_ChangeDir(const char* path) = 0;
+				virtual Alx_Status Client_ChangeDir(const char* path, bool* exists) = 0;
 				virtual Alx_Status Client_MakeDir(const char* path) = 0;
 				virtual Alx_Status Client_UploadFile(const char* localFilePath, const char* remoteFilePath, uint32_t* fileSize, Alx::AlxOsMutex::IAlxOsMutex* alxOsMutex_UploadFileInChunks) = 0;
 				virtual ::AlxFtp* GetCStructPtr(void) = 0;
@@ -136,9 +136,9 @@ namespace Alx
 				{
 					return AlxFtp_Client_Logout(&me);
 				}
-				Alx_Status Client_ChangeDir(const char* path) override
+				Alx_Status Client_ChangeDir(const char* path, bool* exists) override
 				{
-					return AlxFtp_Client_ChangeDir(&me, path);
+					return AlxFtp_Client_ChangeDir(&me, path, exists);
 				}
 				Alx_Status Client_MakeDir(const char* path) override
 				{
